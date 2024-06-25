2ff44c814ff5b57d50645a3b6442e46eb39cf0ec
mohammadKazem <mmadkzm@gmail.com>
2024-06-25 17:26:36 +0330

    modified user request pay entity

--- src/Reservation.Domain/Entities/Finances/UserRequestPay.cs
+++ src/Reservation.Domain/Entities/Finances/UserRequestPay.cs
@@ -3,7 +3,7 @@ namespace Reservation.Domain.Entities.Finances;
 
 public class UserRequestPay : BaseClass
 {
-    public int Price { get; set; }
+    public int Amount { get; set; }
     public bool IsPay { get; set; } = false;
     public DateTime PayDate { get; set; }
     public string Authorizy { get; set; }
@@ -11,7 +11,6 @@ public class UserRequestPay : BaseClass
 
     // Request Pay User
     public User User { get; set; }
+    public Guid UserId { get; set; }
 
-    // Request Pay Reserve
-    public ReserveTimeOut ReserveTime { get; set; }
 }
