5884949d94a8004e208329f0d2f9de87a29d8dab
mohammadKazem <mmadkzm@gmail.com>
2024-06-25 16:22:15 +0330

    added static method To make better and cleaner without route

--- src/Reservation.Application/Businesses/Queries/Search/SearchByAddressRequest.cs
+++ src/Reservation.Application/Businesses/Queries/Search/SearchByAddressRequest.cs
@@ -1,3 +1,7 @@
 namespace Reservation.Application.Businesses.Queries.Search;
 
-public record SearchBusinessRequest(int Page, string Key) : IRequest<IEnumerable<IResponse>>;
+public record SearchBusinessRequest(int Page, string Key) : IRequest<IEnumerable<IResponse>>
+{
+    public static SearchBusinessRequest Create(int page, string key)
+        => new(page, key);
+}
