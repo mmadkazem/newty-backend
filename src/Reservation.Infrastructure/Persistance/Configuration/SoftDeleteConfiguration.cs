b9ee32df765216e60f81319aa52636b79a498826
mohammadKazem <mmadkzm@gmail.com>
2024-07-19 13:18:44 +0330

    added SoftDeleteConfiguration

---
+++ src/Reservation.Infrastructure/Persistance/Configuration/SoftDeleteConfiguration.cs
@@ -0,0 +1,27 @@
+namespace Reservation.Infrastructure.Persistance.Configuration;
+
+
+public static class SoftDeleteConfiguration
+{
+    public static void ConfigSoftDeleteFilter(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<Business>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<Artist>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<Post>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<Service>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<SmsCredit>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<SmsTemplate>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<UserVIP>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<Category>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<BusinessRequestPay>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<UserRequestPay>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<Point>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<ReserveTimeReceipt>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<ReserveTimeSender>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<ReserveItem>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<Wallet>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<Transaction>().HasQueryFilter(f => !f.IsDeleted);
+        modelBuilder.Entity<City>().HasQueryFilter(f => !f.IsDeleted);
+    }
+}
