5b16e980aff3f4f88a6444e264a5100a9de52001
mohammadKazem <mmadkzm@gmail.com>
2024-07-19 12:47:59 +0330

    added block credit in wallet

--- src/Reservation.Domain/Entities/Wallets/Wallet.cs
+++ src/Reservation.Domain/Entities/Wallets/Wallet.cs
@@ -3,6 +3,7 @@ namespace Reservation.Domain.Entities.Wallets;
 public class Wallet : BaseClass
 {
     public decimal Credit { get; set; }
+    public decimal BlockCredit { get; set; }
 
     // Wallet Transactions
     public ICollection<Transaction> Transactions { get; set; } = [];
