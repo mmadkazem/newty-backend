f4fe7d82a373234e95ea7413013c398e977a97d3
mohammadKazem <mmadkzm@gmail.com>
2024-07-19 13:20:47 +0330

    register ITransferFeeRepository to TransferFeeRepository and ISmsPlanRepository to SmsPlanRepository

--- src/Reservation.Infrastructure/Persistance/ConfigureServices.cs
+++ src/Reservation.Infrastructure/Persistance/ConfigureServices.cs
@@ -8,24 +8,28 @@ public static class ConfigureServices
         // DI Repositories
         services.AddTransient<IBusinessRequestPayRepository, BusinessRequestPayRepository>();
         services.AddTransient<IUserRequestPayRepository, UserRequestPayRepository>();
+        services.AddTransient<ITransferFeeRepository, TransferFeeRepository>();
         services.AddTransient<ISmsTemplateRepository, SmsTemplateRepository>();
         services.AddTransient<IReserveTimeRepository, ReserveTimeRepository>();
         services.AddTransient<ISmsCreditRepository, SmsCreditRepository>();
         services.AddTransient<IBusinessRepository, BusinessRepository>();
         services.AddTransient<ICategoryRepository, CategoryRepository>();
         services.AddTransient<IServiceRepository, ServiceRepository>();
+        services.AddTransient<ISmsPlanRepository, SmsPlanRepository>();
         services.AddTransient<IArtistRepository, ArtistRepository>();
         services.AddTransient<IWalletRepository, WalletRepository>();
         services.AddTransient<ICityRepository, CityRepository>();
         services.AddTransient<IUserRepository, UserRepository>();
         services.AddTransient<IPostRepository, PostRepository>();
 
+
         // DI UnitOfWork
         services.AddTransient<IUnitOfWork, UnitOfWork>();
 
         // DI DbContext
         services.AddDbContext<ReservationDbContext>(options =>
-            options.UseNpgsql(configuration.GetConnectionString("ReservationDb")));
+            options.UseNpgsql(configuration.GetConnectionString("NewtyDb")));
+
         return services;
     }
 }
