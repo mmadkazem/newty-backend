bba93d64c899966174c08dc3c82cda4f5184c7ad
mohammadKazem <mmadkzm@gmail.com>
2024-07-30 10:13:33 +0330

    bug fix add service factory scope in method parameter and remove contractor

--- src/Reservation.Infrastructure/ExternalServices/Job/FinishReserveTimeService.cs
+++ src/Reservation.Infrastructure/ExternalServices/Job/FinishReserveTimeService.cs
@@ -1,31 +1,32 @@
 namespace Reservation.Infrastructure.ExternalServices.Job;
 
 
-public sealed class FinishReserveTimeJob(IBackgroundJobClient client) : IFinishReserveTimeJob
+public sealed class FinishReserveTimeJob(IBackgroundJobClient client, IServiceScopeFactory scopeFactory)
+    : IFinishReserveTimeJob
 {
     private readonly IBackgroundJobClient _client = client;
+    private readonly IServiceScopeFactory _scopeFactory = scopeFactory;
+
 
     public void Execute(Guid reserveTimeId, DateTimeOffset date)
     {
-        _client.Schedule<FinishReserveTimeService>(s => s.Execute(reserveTimeId), date);
+        _client.Schedule<FinishReserveTimeService>(s => s.Execute(reserveTimeId, _scopeFactory), date);
     }
 }
 
-public sealed class FinishReserveTimeService(IServiceScopeFactory scopeFactory)
+public sealed class FinishReserveTimeService
 {
-    private readonly IServiceScopeFactory _scopeFactory = scopeFactory;
 
-    public async Task Execute(Guid reserveTimeId)
+    public void Execute(Guid reserveTimeId, IServiceScopeFactory _scopeFactory)
     {
         using var serviceScope = _scopeFactory.CreateScope();
         using var _context = serviceScope.ServiceProvider.GetService<ReservationDbContext>();
 
-        var reserveTime = await _context.ReserveTimesReceipt
-                            .AsQueryable()
-                            .FirstOrDefaultAsync(r => r.Id == reserveTimeId);
+        var reserveTime = _context.ReserveTimesReceipt.AsQueryable()
+                                        .FirstOrDefault(r => r.Id == reserveTimeId);
 
         reserveTime.Finished = true;
 
-        await _context.SaveChangesAsync();
+        _context.SaveChanges();
     }
 }
