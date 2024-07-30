81a54895c1790cdb6cfe37ec9b517a6395784d98
mohammadKazem <mmadkzm@gmail.com>
2024-07-30 10:19:40 +0330

    added base directory path

---
+++ src/Reservation.Infrastructure/Persistance/SeedData/BaseDirectory.cs
@@ -0,0 +1,8 @@
+namespace Reservation.Infrastructure.Persistance.SeedData;
+
+
+public sealed class BaseDirectoryPath
+{
+    public const string InfrastructureDirectoryPath = "F:/Project/Reservation/src/Reservation.Infrastructure/";
+    public const string SeedDataPath = "Persistance/SeedData/Categories/JsonFile/";
+}
